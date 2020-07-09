176e7a8b99c0c6c2a2c7b1d7b7b77b2ef712e072
vmalon <viniciusmalon89@gmail.com>
2020-07-09 15:00:25 -0300

    * Projeto Desafio 1 - Módulo 1 Bootcamp Fullstack

---
+++ script.js
@@ -0,0 +1,32 @@
+window.addEventListener('load',start);
+
+var rangeRed = document.querySelector('#rangeRed');
+var rangeGreen = document.querySelector('#rangeGreen');
+var rangeBlue = document.querySelector('#rangeBlue');
+var inputRed = document.querySelector('#inputRed');
+var inputGreen = document.querySelector('#inputGreen');
+var inputBlue = document.querySelector('#inputBlue');
+
+rangeRed.defaultValue = 0;
+rangeGreen.defaultValue = 0;
+rangeBlue.defaultValue = 0;
+
+
+function start(){
+    rangeRed.addEventListener('mousemove',updateColorRgb);
+    rangeGreen.addEventListener('mousemove',updateColorRgb);
+    rangeBlue.addEventListener('mousemove',updateColorRgb);
+}
+
+function updateColorRgb(){
+    var div = document.querySelector('#divRGB');
+    div.style.backgroundColor = 'rgb(' + rangeRed.value + ',' +
+    + rangeGreen.value + ',' +
+    + rangeBlue.value 
+    +')';
+
+    inputRed.value = rangeRed.value;
+    inputGreen.value = rangeGreen.value;
+    inputBlue.value = rangeBlue.value;
+    
+}
